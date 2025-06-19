f69a64752d6b871a1ef2de671cb403911924ab01
Alexey <mr.swasher@gmail.com>
2025-06-19 11:30:45 +0300

    bump

--- moviefilter/htmx_views.py
+++ moviefilter/htmx_views.py
@@ -231,9 +231,6 @@ def rss_table_data(request):
     return render(request, 'partials/rss-table.html', context)
 
 
-from django.views.decorators.csrf import csrf_exempt
-
-
 @require_POST
 def ignore_movie(request, pk):
     try:
